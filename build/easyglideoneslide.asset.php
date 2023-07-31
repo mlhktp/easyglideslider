8b9638ae5fd5c7cc0ba1da9440e00adebbf6528f
mlhktp <aktopmelih@gmail.com>
2023-07-31 17:56:12 +0200

    Delete build folder

--- build/easyglideoneslide.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => 'a7f60b3aa40d3cc6fe76');
--- build/easyglideslidermain.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element'), 'version' => '9c1b2c536ebd2539acd8');
